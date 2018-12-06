c227104e932945befd871bd0defd5b276be6c3be
febc.yamamoto <yamamoto.febc@gmail.com>
2018-12-06 11:34:08 +0900

    change ssh_private_key output path

--- keys.tf
+++ keys.tf
@@ -7,5 +7,5 @@ resource "tls_private_key" "ssh_key" {
 
 resource "local_file" "ssh_key" {
   content  = "${tls_private_key.ssh_key.private_key_pem}"
-  filename = "${path.root}/certs/id_rsa"
+  filename = "${path.root}/id_rsa"
 }
